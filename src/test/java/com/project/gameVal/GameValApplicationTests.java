f521575619664c1693ce27ed5083797833ae4651
siwoo <k74415834@gmail.com>
2024-07-19 14:26:35 +0900

    init: init project

---
+++ src/test/java/com/project/gameVal/GameValApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.gameVal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameValApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
