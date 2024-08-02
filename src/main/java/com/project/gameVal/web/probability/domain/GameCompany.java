542c0546de4c36cad087a2fa7272e354f3d13f9e
haebyun <bhk4392@gmail.com>
2024-08-03 00:47:06 +0900

    feat : GameCompany 엔티티 클래스 추가
    - 게임 회사 정보를 저장하기 위한 GameCompany 엔티티 클래스 생성
    - 필드: id, name, password, role, games
    - JPA 어노테이션을 사용하여 데이터베이스 테이블과 매핑
    - 게임과의 일대다 관계 설정

---
+++ src/main/java/com/project/gameVal/web/probability/domain/GameCompany.java
@@ -0,0 +1,44 @@
+package com.project.gameVal.web.probability.domain;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "game_company")
+public class GameCompany {
+    @Id
+    @Column(nullable = false, name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToMany(mappedBy = "gameCompany", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<Game> games;
+
+}
