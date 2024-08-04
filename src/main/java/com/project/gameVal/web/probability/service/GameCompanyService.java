4393618c2c32f87ca9cb58dc1851432acbf9fc8c
haebyun <bhk4392@gmail.com>
2024-08-04 17:09:42 +0900

    refactor : GameCompanyService 인터페이스의 findById 메서드 반환 타입 변경
    - findById 메서드의 반환 타입을 Optional<GameCompany>에서 GameCompany로 변경
    - 메서드 사용 시 가독성을 높이고, null 처리에 대한 책임을 호출측으로 이전

--- src/main/java/com/project/gameVal/web/probability/service/GameCompanyService.java
+++ src/main/java/com/project/gameVal/web/probability/service/GameCompanyService.java
@@ -3,7 +3,6 @@ package com.project.gameVal.web.probability.service;
 
 import com.project.gameVal.web.probability.domain.GameCompany;
 import com.project.gameVal.web.probability.dto.GameCompanyRegisterDTO;
-import java.util.Optional;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface GameCompanyService extends UserDetailsService {
@@ -11,7 +10,7 @@ public interface GameCompanyService extends UserDetailsService {
 
     Long findIdByName(String gameCompanyName);
 
-    Optional<GameCompany> findById(Long id);
+    GameCompany findById(Long id);
 
     void logout(String accessToken);
 }
