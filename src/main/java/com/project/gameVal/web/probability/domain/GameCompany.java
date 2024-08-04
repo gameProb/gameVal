af1dd8d5f037174606b80ce096957fe0fd28631c
haebyun <bhk4392@gmail.com>
2024-08-04 14:55:27 +0900

    refactor : GameCompany 엔티티 필드 유효성 검사 추가
    - name 필드에 최소 길이 4자, 최대 길이 20자 제약 조건 추가
    - password 필드에 최소 길이 4자, 최대 길이 30자 제약 조건 추가
    - 엔티티의 데이터 유효성을 강화하여 입력값 검증 수행

--- src/main/java/com/project/gameVal/web/probability/domain/GameCompany.java
+++ src/main/java/com/project/gameVal/web/probability/domain/GameCompany.java
@@ -11,6 +11,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -29,9 +32,11 @@ public class GameCompany {
     private Long id;
 
     @Column(nullable = false)
+    @NotBlank @Min(4) @Max(20)
     private String name;
 
     @Column(nullable = false)
+    @NotBlank @Min(4) @Max(30)
     private String password;
 
     @Enumerated(EnumType.STRING)
