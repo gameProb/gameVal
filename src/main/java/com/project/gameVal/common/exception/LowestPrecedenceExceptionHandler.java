38dbf9934ad57764aaa7c9c989b27d06c1364eab
haebyun <bhk4392@gmail.com>
2024-08-05 22:14:33 +0900

    refactor: Unhandled Error 처리기 제거
    - Exception.class에 대한 @ExceptionHandler 메서드 제거
    - 이전에 잡히지 않은 모든 예외를 INTERNAL_SERVER_ERROR으로 처리하는 것이 클라이언트에게 정확한 오류 정보를 제공하지 못한다고 판단하여 수정

--- src/main/java/com/project/gameVal/common/exception/LowestPrecedenceExceptionHandler.java
+++ src/main/java/com/project/gameVal/common/exception/LowestPrecedenceExceptionHandler.java
@@ -24,10 +24,4 @@ public class LowestPrecedenceExceptionHandler {
         log.warn("Illegal Argument Exception", ex);
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> unHandledError(Exception ex) {
-        log.error("Unhandled Error", ex);
-        return new ResponseEntity<>("예상치 못한 에러가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
-    }
 }
