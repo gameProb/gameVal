135e89b2ab24dc1706d468accbaa28d2d7c70dc7
haebyun <bhk4392@gmail.com>
2024-09-04 20:05:27 +0900

    fix : ProbabilityTableDTO의 리스트에 잘못된 @NotBalnk 어노테이션 사용 수정

--- src/main/java/com/project/gameVal/web/probability/dto/ProbabilityTableDTO.java
+++ src/main/java/com/project/gameVal/web/probability/dto/ProbabilityTableDTO.java
@@ -3,6 +3,7 @@ package com.project.gameVal.web.probability.dto;
 import com.project.gameVal.web.probability.domain.ProbabilityPair;
 import com.project.gameVal.web.probability.domain.ProbabilityTable;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,7 +16,7 @@ public class ProbabilityTableDTO {
     @NotBlank
     private String name;
 
-    @NotBlank
+    @NotEmpty(message = "Probabilities list must not be empty")
     private List<ProbabilityPair> probabilities;
 
     public ProbabilityTable toEntity(Long gameCompanyId) {
