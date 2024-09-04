ded8982b84e87992b510ef83327235c3f7c9642c
haebyun <bhk4392@gmail.com>
2024-09-04 20:06:35 +0900

    feat : ProbabilityTableDTO와 ProbabilityPair에 검증 관련 어노테이션 추가

--- src/main/java/com/project/gameVal/web/probability/dto/ProbabilityTableDTO.java
+++ src/main/java/com/project/gameVal/web/probability/dto/ProbabilityTableDTO.java
@@ -2,8 +2,10 @@ package com.project.gameVal.web.probability.dto;
 
 import com.project.gameVal.web.probability.domain.ProbabilityPair;
 import com.project.gameVal.web.probability.domain.ProbabilityTable;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,6 +19,8 @@ public class ProbabilityTableDTO {
     private String name;
 
     @NotEmpty(message = "Probabilities list must not be empty")
+    @Size(min = 1, message = "Probabilities list must contain at least one element")
+    @Valid
     private List<ProbabilityPair> probabilities;
 
     public ProbabilityTable toEntity(Long gameCompanyId) {
