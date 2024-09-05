ec579d6d0f0b73103a95e471701e8de84188daf7
siwoo <k74415834@gmail.com>
2024-09-05 12:55:02 +0900

    fix: 확률 테이블 controller에서 @AuthenticationPrincipal로 게임사 정보를 가져오도록 변경

--- src/main/java/com/project/gameVal/web/probability/controller/api/ProbabilityTableAPIController.java
+++ src/main/java/com/project/gameVal/web/probability/controller/api/ProbabilityTableAPIController.java
@@ -1,6 +1,6 @@
 package com.project.gameVal.web.probability.controller.api;
 
-import com.project.gameVal.common.jwt.auth.JWTUtil;
+import com.project.gameVal.web.probability.domain.GameCompanyInformInToken;
 import com.project.gameVal.web.probability.domain.ProbabilityTable;
 import com.project.gameVal.web.probability.dto.ProbabilityTableDTO;
 import com.project.gameVal.web.probability.dto.ResultRequestDTO;
@@ -10,13 +10,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/probability/api")
@@ -24,14 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class ProbabilityTableAPIController {
     private final ProbabilityTableService probabilityTableService;
-    private final JWTUtil jwtUtil;
 
     @PostMapping("/table/add")
-    public ResponseEntity<String> addTable(@RequestHeader("Authorization") String authorizationHeader, @Valid @RequestBody ProbabilityTableDTO probabilityTableDTO) {
+    public ResponseEntity<String> addTable(@AuthenticationPrincipal GameCompanyInformInToken gameCompanyInform, @Valid @RequestBody ProbabilityTableDTO probabilityTableDTO) {
         log.info("start add table");
-        String accessToken = jwtUtil.getAccessTokenByAuthorizationHeader(authorizationHeader);
-        Long gameCompanyId = jwtUtil.getIdByAccessToken(accessToken);
-        ProbabilityTable savedProbabilityTable = probabilityTableService.createProbabilityTable(gameCompanyId, probabilityTableDTO);
+
+        ProbabilityTable savedProbabilityTable = probabilityTableService.createProbabilityTable(gameCompanyInform.getId(), probabilityTableDTO);
         return new ResponseEntity<>("successfully probability table added and id is " + savedProbabilityTable.getId(), HttpStatus.CREATED);
     }
 
